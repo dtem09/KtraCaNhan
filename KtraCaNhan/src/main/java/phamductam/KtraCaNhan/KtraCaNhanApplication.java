d4f043a555f727a5e982e1e0b9bf9095526f2945
minah <minaheo7@gmail.com>
2023-06-05 00:57:45 +0700

    KiemTra

---
+++ KtraCaNhan/src/main/java/phamductam/KtraCaNhan/KtraCaNhanApplication.java
@@ -0,0 +1,13 @@
+package phamductam.KtraCaNhan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KtraCaNhanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KtraCaNhanApplication.class, args);
+	}
+
+}
